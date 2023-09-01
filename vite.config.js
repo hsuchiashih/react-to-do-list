9666dfae5fa4a8e8099485d06545b8bc289f7649
hsuchiashih <hsuchiashih@gmail.com>
2023-09-01 17:00:33 +0000

    gh-page 設定

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/react-to-do-list/' : '/',
   plugins: [react()],
 })
